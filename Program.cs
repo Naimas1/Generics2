7841afdc4649a80d083d6c3a5c4b4ce194021a91
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-13 18:31:30 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,49 @@
-﻿namespace Generics2
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics2
 {
-    internal class Program
+    public class Alphabet : IEnumerable<char>
     {
-        static void Main(string[] args)
+        private readonly char[] letters;
+
+        public Alphabet()
         {
-            Console.WriteLine("Hello, World!");
+            letters = new char[26];
+            for (int i = 0; i < 26; i++)
+            {
+                letters[i] = (char)('A' + i);
+            }
+        }
+
+        public IEnumerator<char> GetEnumerator()
+        {
+            foreach (char letter in letters)
+            {
+                yield return letter;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class Programzx
+    {
+        public static void Main(string[] args)
+        {
+            Alphabet alphabet = new Alphabet();
+
+            foreach (char letter in alphabet)
+            {
+                Console.WriteLine(letter);
+            }
         }
     }
 }
