094ebe4c81cd1d2941d66177c7091d0ed4004dd5
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-13 18:23:11 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Generics2
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics2
 {
-    internal class Program
+    public class Player
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+
+    public class FootballTeam : IEnumerable<Player>
+    {
+        private List<Player> players = new List<Player>();
+
+        public void AddPlayer(Player player)
         {
-            Console.WriteLine("Hello, World!");
+            players.Add(player);
+        }
+
+        public IEnumerator<Player> GetEnumerator()
+        {
+            return players.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class Programsq
+    {
+        public static void Main(string[] args)
+        {
+            FootballTeam team = new FootballTeam();
+            team.AddPlayer(new Player { Name = "Neimar", Number = 10 });
+            team.AddPlayer(new Player { Name = "Messi", Number = 7 });
+            team.AddPlayer(new Player { Name = "Pele", Number = 4 });
+
+            foreach (Player player in team)
+            {
+                Console.WriteLine("Name: " + player.Name + ", Number: " + player.Number);
+            }
         }
     }
 }
