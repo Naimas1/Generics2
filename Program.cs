5fff315bd0b7895fae36dbf32d77214346f8f121
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-13 18:26:02 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Generics2
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics2
 {
-    internal class Program
+    public class CafeEmployee
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+        public string Position { get; set; }
+    }
+
+    public class Cafe : IEnumerable<CafeEmployee>
+    {
+        private List<CafeEmployee> employees = new List<CafeEmployee>();
+
+        public void AddEmployee(CafeEmployee employee)
         {
-            Console.WriteLine("Hello, World!");
+            employees.Add(employee);
+        }
+
+        public IEnumerator<CafeEmployee> GetEnumerator()
+        {
+            return employees.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class Programx
+    {
+        public static void Main(string[] args)
+        {
+            Cafe cafe = new Cafe();
+            cafe.AddEmployee(new CafeEmployee { Name = "John", Position = "Barista" });
+            cafe.AddEmployee(new CafeEmployee { Name = "Emily", Position = "Waitress" });
+            cafe.AddEmployee(new CafeEmployee { Name = "Mike", Position = "Chef" });
+
+            foreach (CafeEmployee employee in cafe)
+            {
+                Console.WriteLine("Name: " + employee.Name + ", Position: " + employee.Position);
+            }
         }
     }
 }
