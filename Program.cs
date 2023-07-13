221bcd8996256500e9dc6141e8b5c6dfddce74b4
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-13 18:18:32 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,57 @@
-﻿namespace Generics2
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics2
 {
-    internal class Program
+    public class MarineCreature
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+
+        public MarineCreature(string name)
         {
-            Console.WriteLine("Hello, World!");
+            Name = name;
+        }
+    }
+    public class Oceanarium : IEnumerable<MarineCreature>
+    {
+        private List<MarineCreature> inhabitants;
+
+        public Oceanarium()
+        {
+            inhabitants = new List<MarineCreature>();
+        }
+        public void AddInhabitant(MarineCreature creature)
+        {
+            inhabitants.Add(creature);
+        }
+        public IEnumerator<MarineCreature> GetEnumerator()
+        {
+            return inhabitants.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    public class Programc
+    {
+        public static void Main(string[] args)
+        {
+            Oceanarium oceanarium = new Oceanarium();
+
+            oceanarium.AddInhabitant(new MarineCreature("Дельфін"));
+            oceanarium.AddInhabitant(new MarineCreature("Акула"));
+            oceanarium.AddInhabitant(new MarineCreature("Кит"));
+
+            foreach (MarineCreature creature in oceanarium)
+            {
+                Console.WriteLine(creature.Name);
+            }
         }
     }
 }
